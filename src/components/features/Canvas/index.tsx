94b059affc9c92da0ddc31a78712e14419e24cb6
Danylo Hatak <dhatak24@gmail.com>
2026-05-02 02:25:19 +0300

    feat: added fixed point canvas mouse navigation
    Now you can move through canvas using cursor postion on mouse drag and release

--- src/components/features/Canvas/index.tsx
+++ src/components/features/Canvas/index.tsx
@@ -8,6 +8,56 @@ const CIRCLE_GAP = 30;
 
 const CanvasComponent = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const defCanvasCoordsRef = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
+  const defMouseCoordsRef = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
+  const mouseOffsetRef = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
+  const isDraggableRef = useRef<boolean>(false);
+  const rafRef = useRef<number>(0);
+
+  const setMouseCoords = (event: MouseEvent) => {
+    const x = event?.pageX;
+    const y = event?.pageY;
+
+    defMouseCoordsRef.current = { x, y };
+  };
+
+  const setCanvasCoords = () => {
+    console.log({ "mouseOffsetRef?.current?.x": mouseOffsetRef?.current?.x });
+    console.log({ "mouseOffsetRef?.current?.y": mouseOffsetRef?.current?.y });
+    console.log({ "defCanvasCoordsRef?.current?.x": defCanvasCoordsRef?.current?.x });
+    console.log({ "defCanvasCoordsRef?.current?.y": defCanvasCoordsRef?.current?.y });
+    if (
+      mouseOffsetRef?.current?.x === defCanvasCoordsRef?.current?.x &&
+      mouseOffsetRef?.current?.y === defCanvasCoordsRef?.current?.y
+    ) {
+      return;
+    }
+    const x = mouseOffsetRef?.current?.x;
+    const y = mouseOffsetRef?.current?.y;
+
+    defCanvasCoordsRef.current = { x, y };
+  };
+
+  const onMouseMove = (event: MouseEvent) => {
+    rafRef.current = requestAnimationFrame(() => {
+      calculateMouseOffset(event);
+    });
+  };
+
+  const calculateMouseOffset = (event: MouseEvent) => {
+    const currX = event?.pageX;
+    const currY = event?.pageY;
+
+    const x = defMouseCoordsRef?.current?.x + defCanvasCoordsRef?.current?.x - currX;
+    const y = defMouseCoordsRef?.current?.y + defCanvasCoordsRef?.current?.y - currY;
+    mouseOffsetRef.current = { x, y };
+    cancelAnimationFrame(rafRef.current);
+    rafRef.current = requestAnimationFrame(draw);
+  };
+
+  // const resetMouseOffset = () => {
+  //   mouseOffsetRef.current = { x: 0, y: 0 };
+  // };
 
   const draw = useCallback(() => {
     const canvas = canvasRef.current;
@@ -17,7 +67,16 @@ const CanvasComponent = () => {
     canvas.height = canvas.offsetHeight;
 
     const ctx = canvas.getContext("2d");
+
     if (!ctx) return;
+
+    if (isDraggableRef?.current && mouseOffsetRef?.current) {
+      const x = mouseOffsetRef?.current?.x;
+      const y = mouseOffsetRef?.current?.y;
+
+      ctx.resetTransform();
+      ctx.translate(x, y);
+    }
 
     ctx.fillStyle = CANVAS_COLOR_BG;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -36,11 +95,23 @@ const CanvasComponent = () => {
         ctx.fill();
       }
     }
-  }, []);
+  }, [defCanvasCoordsRef?.current, defMouseCoordsRef?.current, mouseOffsetRef.current]);
 
   useEffect(() => {
     draw();
     window.addEventListener("resize", draw);
+    window.addEventListener("mousedown", (event: MouseEvent) => {
+      isDraggableRef.current = true;
+      setMouseCoords(event);
+      window.addEventListener("mousemove", onMouseMove);
+    });
+    window.addEventListener("mouseup", () => {
+      // window.addEventListener("mouseup", (event: MouseEvent) => {
+      isDraggableRef.current = false;
+      setCanvasCoords();
+      window.removeEventListener("mousemove", onMouseMove);
+    });
+
     return () => window.removeEventListener("resize", draw);
   }, [draw]);
 
